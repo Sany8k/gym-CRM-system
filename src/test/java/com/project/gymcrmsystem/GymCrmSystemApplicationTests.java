397ddaa6d0846c17c1bedc3d0bed91d1fa269568
Alex <sashatsapko91@gmail.com>
2026-08-28 18:00:29 +0300

    first commit

---
+++ src/test/java/com/project/gymcrmsystem/GymCrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gymcrmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
