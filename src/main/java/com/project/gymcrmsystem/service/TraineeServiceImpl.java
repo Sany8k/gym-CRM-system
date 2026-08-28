71a5e749318f88568bfd31f0de1af38cab722468
Alex <sashatsapko91@gmail.com>
2026-08-29 01:55:28 +0300

    refactor Trainee and Trainer services to use PasswordGenerator for password creation; remove redundant password generation logic

--- src/main/java/com/project/gymcrmsystem/service/TraineeServiceImpl.java
+++ src/main/java/com/project/gymcrmsystem/service/TraineeServiceImpl.java
@@ -3,6 +3,7 @@ package com.project.gymcrmsystem.service;
 import com.project.gymcrmsystem.dao.TraineeDao;
 import com.project.gymcrmsystem.dao.TrainerDao;
 import com.project.gymcrmsystem.model.Trainee;
+import com.project.gymcrmsystem.util.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,21 @@ import java.util.Optional;
 public class TraineeServiceImpl implements TraineeService {
   private TraineeDao traineeDao;
   private TrainerDao trainerDao;
-
-  private static final String PASSWORD_SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-      "abcdefghijklmnopqrstuvwxyz" +
-      "0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
-  private static final SecureRandom RANDOM = new SecureRandom();
+  private PasswordGenerator passwordGenerator;
 
   @Autowired
-  public void setTraineeDao(TraineeDao traineeDao, TrainerDao trainerDao) {
+  public void setTraineeDao(TraineeDao traineeDao) {
     this.traineeDao = traineeDao;
+  }
+
+  @Autowired
+  public void setTrainerDao(TrainerDao trainerDao) {
     this.trainerDao = trainerDao;
+  }
+
+  @Autowired
+  public void setPasswordGenerator(PasswordGenerator passwordGenerator) {
+    this.passwordGenerator = passwordGenerator;
   }
 
   @Override
@@ -46,7 +52,7 @@ public class TraineeServiceImpl implements TraineeService {
       counter++;
     }
     trainee.setUsername(username);
-    trainee.setPassword(generateRandomPassword());
+    trainee.setPassword(passwordGenerator.generateRandomPassword());
     return traineeDao.save(trainee);
   }
 
@@ -62,16 +68,5 @@ public class TraineeServiceImpl implements TraineeService {
 
   private boolean isUsernameTaken(String username) {
     return traineeDao.findByUsername(username).isPresent() || trainerDao.findByUsername(username).isPresent();
-  }
-
-  private String generateRandomPassword() {
-    StringBuilder password = new StringBuilder(10);
-
-    for (int i = 0; i < 10; i++) {
-      int index = RANDOM.nextInt(PASSWORD_SYMBOLS.length());
-      password.append(PASSWORD_SYMBOLS.charAt(index));
-    }
-
-    return password.toString();
   }
 }
--- src/main/java/com/project/gymcrmsystem/service/TrainerServiceImpl.java
+++ src/main/java/com/project/gymcrmsystem/service/TrainerServiceImpl.java
@@ -3,6 +3,7 @@ package com.project.gymcrmsystem.service;
 import com.project.gymcrmsystem.dao.TraineeDao;
 import com.project.gymcrmsystem.dao.TrainerDao;
 import com.project.gymcrmsystem.model.Trainer;
+import com.project.gymcrmsystem.util.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,21 @@ import java.util.Optional;
 public class TrainerServiceImpl implements TrainerService {
   private TrainerDao trainerDao;
   private TraineeDao traineeDao;
-
-  private static final String PASSWORD_SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-      "abcdefghijklmnopqrstuvwxyz" +
-      "0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
-  private static final SecureRandom RANDOM = new SecureRandom();
+  private PasswordGenerator passwordGenerator;
 
   @Autowired
-  public void setTrainerDao(TrainerDao trainerDao, TraineeDao traineeDao) {
+  public void setTrainerDao(TrainerDao trainerDao) {
     this.trainerDao = trainerDao;
+  }
+
+  @Autowired
+  public void setTraineeDao(TraineeDao traineeDao) {
     this.traineeDao = traineeDao;
+  }
+
+  @Autowired
+  public void setPasswordGenerator(PasswordGenerator passwordGenerator) {
+    this.passwordGenerator = passwordGenerator;
   }
 
   @Override
@@ -46,7 +52,7 @@ public class TrainerServiceImpl implements TrainerService {
       counter++;
     }
     trainer.setUsername(username);
-    trainer.setPassword(generateRandomPassword());
+    trainer.setPassword(passwordGenerator.generateRandomPassword());
     return trainerDao.save(trainer);
   }
 
@@ -57,16 +63,5 @@ public class TrainerServiceImpl implements TrainerService {
 
   private boolean isUsernameTaken(String username) {
     return traineeDao.findByUsername(username).isPresent() || trainerDao.findByUsername(username).isPresent();
-  }
-
-  private String generateRandomPassword() {
-    StringBuilder password = new StringBuilder(10);
-
-    for (int i = 0; i < 10; i++) {
-      int index = RANDOM.nextInt(PASSWORD_SYMBOLS.length());
-      password.append(PASSWORD_SYMBOLS.charAt(index));
-    }
-
-    return password.toString();
   }
 }
